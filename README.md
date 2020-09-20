ac2e2871b9959bff734b7a2f3edd66da68fdf3a1
raywu <wuhaoshiang1992@gmail.com>
2020-09-21 00:04:52 +0800

    add reference

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ https://medium.com/@ckwang19/implement-the-backpropagation-with-python-step-by-s
 https://medium.com/@14prakash/back-propagation-is-very-simple-who-made-it-complicated-97b794c97e5c
 https://github.com/PytLab/simpleflow
 https://towardsdatascience.com/backpropagation-in-a-convolutional-layer-24c8d64d8509
+https://medium.com/@chih.sheng.huang821/%E5%8D%B7%E7%A9%8D%E7%A5%9E%E7%B6%93%E7%B6%B2%E8%B7%AF-convolutional-neural-network-cnn-%E5%8D%B7%E7%A9%8D%E8%A8%88%E7%AE%97%E4%B8%AD%E7%9A%84%E6%AD%A5%E4%BC%90-stride-%E5%92%8C%E5%A1%AB%E5%85%85-padding-94449e638e82
--- src/operations/convolution.py
+++ src/operations/convolution.py
@@ -17,6 +17,11 @@ class Cov1d(Operation):
             self.object = np.random.randn(kernel_size)
 
     def get_output(self):
+        # add padding on input
+
+        # calculate output shape
+
+        # fill the output
         pass
 
     def get_gradient(self):
@@ -24,8 +29,37 @@ class Cov1d(Operation):
 
 
 class Cov2d(Operation):
-    pass
+    def __init__(self, input, padding, stride, kernel_size, name, initializer):
+        "padding can be 'SAME' or 'VALID'"
+        if padding == 'VALID':
+            self.input = self.padding_input(input)
+        else:
+            self.input = input
+        self.padding = padding
+        self.stride = stride
+        self.kernel_size = kernel_size
+        self.name = name
+        self.initializer = initializer
+        if initializer == "random_normal":
+
+            self.filter = np.random.randn(kernel_size)
+        else:
+            self.filter = np.random.randn(kernel_size)
+
+    def get_output(self):
+        # calculate output shape
+
+        # fill the output
+
+    def get_gradient(self):
+        pass
+
+    @staticmethod
+    def padding_input(self, input, kernel_size):
+        padding_shape = 123
+        pass
 
 
 class Cov3d(Operation):
+    # TODO
     pass
