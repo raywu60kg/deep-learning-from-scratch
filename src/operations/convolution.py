90aedf1121c21fc7bc6650a8075d7b2e18be8985
raywu <wuhaoshiang1992@gmail.com>
2020-09-22 23:35:43 +0800

    add padding input

--- src/operations/convolution.py
+++ src/operations/convolution.py
@@ -3,18 +3,18 @@ import numpy as np
 
 
 class Cov1d(Operation):
-    def __init__(self, input, padding, stride, kernel_size, name, initializer):
+    def __init__(self, input, padding, stride, filter_size, name, initializer):
         self.input = input
         self.padding = padding
         self.stride = stride
-        self.kernel_size = kernel_size
+        self.filter_size = filter_size
         self.name = name
         self.initializer = initializer
         if initializer == "random_normal":
 
-            self.object = np.random.randn(kernel_size)
+            self.object = np.random.randn(filter_size)
         else:
-            self.object = np.random.randn(kernel_size)
+            self.object = np.random.randn(filter_size)
 
     def get_output(self):
         # add padding on input
@@ -29,35 +29,43 @@ class Cov1d(Operation):
 
 
 class Cov2d(Operation):
-    def __init__(self, input, padding, stride, kernel_size, name, initializer):
+    def __init__(self, input, padding, stride, filter_size, name, initializer):
         "padding can be 'SAME' or 'VALID'"
         if padding == 'VALID':
-            self.input = self.padding_input(input)
+            self.padding_input = self.get_padding_input(input)
         else:
-            self.input = input
+            self.padding_input = input
         self.padding = padding
         self.stride = stride
-        self.kernel_size = kernel_size
+        self.filter_size = filter_size
         self.name = name
         self.initializer = initializer
         if initializer == "random_normal":
-
-            self.filter = np.random.randn(kernel_size)
+            self.filter = np.random.randn(filter_size)
         else:
-            self.filter = np.random.randn(kernel_size)
+            self.filter = np.random.randn(filter_size)
 
     def get_output(self):
         # calculate output shape
+        #  input_size + 2 * padding_size-(filter_size-1)
 
         # fill the output
+        pass
 
     def get_gradient(self):
         pass
 
     @staticmethod
-    def padding_input(self, input, kernel_size):
-        padding_shape = 123
-        pass
+    def padding_input(input, filter_size, stride):
+        # assume filter_size is square
+        # padding_size = (filter_size-1) / 2
+        padding_shape = (filter_size[0]-1)/2
+        tmp = tuple(map(lambda x: x+padding_shape, np.shape(input)))
+        print("@@@@", tmp)
+        paddding_input = np.zeros(tmp)
+        paddding_input[padding_shape:, padding_shape:] = input
+
+        return padding_shape
 
 
 class Cov3d(Operation):
--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -1,7 +1,17 @@
 from src.operations.convolution import Cov1d, Cov2d, Cov3d
 import numpy as np
 
+
 class TestConvolution:
     def test_cov1d(self):
         print(np.random.randn(3))
+        assert 1 == 1
+
+    def test_cov2d_padding_input(self):
+        print(tuple(map(lambda x: x+1, (1,2))))
+        res = Cov2d.padding_input(
+            input=np.array([range(9)]).reshape([3, 3]),
+            filter_size=[2, 2],
+            stride=1)
+        print(res)
         assert 1 == 2
